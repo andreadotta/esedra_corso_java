4b43d50227bb611eb89c8c2b43eee4dfa0c1607f
pc <pc@DESKTOP-MFS6HC4.station>
2020-06-20 17:31:28 +0200

    sto facendo corso

---
+++ CorsoJava/src/it/esedra/corso/oussein/Corso.java
@@ -0,0 +1,14 @@
+package it.esedra.corso.oussein;
+
+public class Corso {
+
+	public Corso() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
