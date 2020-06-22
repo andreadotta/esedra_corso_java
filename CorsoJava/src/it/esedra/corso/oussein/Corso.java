d92f4391fedf27034f4a689c72bdc219dd565527
pc <pc@DESKTOP-MFS6HC4.station>
2020-06-22 19:46:11 +0200

    esercio 1 oussein

--- CorsoJava/src/it/esedra/corso/oussein/Corso.java
+++ CorsoJava/src/it/esedra/corso/oussein/Corso.java
@@ -3,12 +3,46 @@ package it.esedra.corso.oussein;
 public class Corso {
 
 	public Corso() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String nome = "ousseynou";
+		System.out.println("il mio nome e " + nome);
+
+		String congnome = "Mbengue";
+		System.out.println("il mio congnome e " + congnome);
+
+		String cittadinascita = "Agliana";
+		System.out.println("la mia Citta di Nascita e " + cittadinascita);
+
+		int eta = 20;
+
+		double altezza = 1.98;
+		System.out.println("la mia Altezza e " + altezza);
+
+		String genere = "Maschio";
+
+		if (eta > 18) {
+			System.out.println("sei maggiorenne");
+		} else {
+			System.out.println("sei minorenne");
+		}
+
+		switch (genere) {
+		case ("Maschio"):
+			System.out.println("l utente e Maschio");
+			break;
+		case ("femmina"):
+
+			System.out.println("l utente e femmina");
+		}
+	}
+
+
+
+
 
 	}
 
-}
+
