a5b9b6ee262ea43b9df5e32273e49bc1aa80e8b4
pc <pc@DESKTOP-MFS6HC4.station>
2020-06-23 12:33:58 +0200

    tablebis

---
+++ CorsoJava/src/it/esedra/corso/oussein/Tablebis.java
@@ -0,0 +1,39 @@
+package it.esedra.corso.oussein;
+
+
+
+
+		import java.awt.BorderLayout;
+		import javax.swing.JFrame;
+		import javax.swing.JTable;
+
+
+		public class Tablebis {
+
+		     public static void main(String[] args) {
+
+		    JFrame Frame= new JFrame("JTable Demo");
+		    String[] tittles= { "id","Materiels informatiques", "quantites"};
+		    
+		    Object[] [] data= {
+		  
+		    		{1,"PC","30"},
+		    		{2,"imprimantes","45"},
+		    		{3,"Tablettes","12"},
+		    };
+		    
+		    JTable table=new JTable(data,tittles); 
+		 
+		 Frame.getContentPane().add(table.getTableHeader(),BorderLayout.NORTH);
+		 Frame.getContentPane().add(table);
+		    
+		    Frame.setSize(450,250);
+		    Frame.setVisible(true);
+		  
+		    
+			}
+
+		
+	}
+
+
