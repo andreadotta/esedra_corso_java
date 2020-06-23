88a1c54e8753c6cbafa2d3e6cf3268ce76f53497
JesooBamboo <JesooBamboo@DESKTOP-151G4H0.station>
2020-06-23 12:16:51 +0200

    Prova su frazioni e somma di due frazioni

--- CorsoJava/src/it/esedra/corso/marco/Corso.java
+++ CorsoJava/src/it/esedra/corso/marco/Corso.java
@@ -3,9 +3,16 @@ package it.esedra.corso.marco;
 public class Corso {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/*
+		 * Siamo nella Classe principale, dove posso richiamare i metodi della classe Frazioni per
+		 * dichiarare le due frazioni da sommare e per stampare a schermo il risultato
+		 */
 		
-	}
+		Frazioni f1 = new Frazioni(2, 3);
+		Frazioni Somma = f1.Somma(new Frazioni(4, 6));
+		
+		System.out.println("La somma è: " + Somma.Visualizzatore());		
+		
+	}	
 
 }
