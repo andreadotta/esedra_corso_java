efb84babc03c41d7bab7310ed9eeda67e1992075
JesooBamboo <JesooBamboo@DESKTOP-151G4H0.station>
2020-06-24 14:06:13 +0200

    reset dell'esercitazione, riprovo da zero

--- CorsoJava/src/it/esedra/corso/marco/Corso.java
+++ CorsoJava/src/it/esedra/corso/marco/Corso.java
@@ -3,15 +3,6 @@ package it.esedra.corso.marco;
 public class Corso {
 
 	public static void main(String[] args) {
-		/*
-		 * Siamo nella Classe principale, dove posso richiamare i metodi della classe Frazioni per
-		 * dichiarare le due frazioni da sommare e per stampare a schermo il risultato
-		 */
-		
-		Frazioni f1 = new Frazioni(2, 3);
-		Frazioni Somma = f1.Somma(new Frazioni(4, 6));
-		
-		System.out.println("La somma è: " + Somma.Visualizzatore());		
 		
 	}	
 
