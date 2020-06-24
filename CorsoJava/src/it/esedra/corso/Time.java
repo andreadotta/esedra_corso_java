38277f4a91ec11ee89bddf7e828d7100160d462f
bauhausk <andrea@xkoll.com>
2020-06-24 18:04:15 +0200

    Esercizio time

---
+++ CorsoJava/src/it/esedra/corso/Time.java
@@ -0,0 +1,18 @@
+package it.esedra.corso;
+
+/**
+ * In questa classe dovete creare 3 variabili di classe (proprietà)
+ * 
+ * hours
+ * minutes
+ * seconds
+ * 
+ * 
+ * Quindi create un costruttore  che inizializzi queste variabili a 0.
+ * @author bauhausk
+ *
+ */
+public class Time {
+
+	
+}
