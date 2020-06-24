568dffa8ecfe13264130b74454fc4a9a83cec5fb
pc <pc@DESKTOP-MFS6HC4.station>
2020-06-24 19:28:56 +0200

    time

---
+++ CorsoJava/src/it/esedra/corso/oussein/Time.java
@@ -0,0 +1,43 @@
+package it.esedra.corso.oussein;
+
+public class Time {
+
+	private int hours;
+	private int minutes;
+	private int seconds;
+
+	public void setTime(int h, int mn, int s) {
+		
+		if (h < 24 && h >= 0) {
+			this.hours = h;
+		} else {
+			this.hours = 0;
+		}
+		if (h < 60 && h >= 0) {
+			this.minutes = mn;
+		} else {
+			this.minutes = 0;
+		}
+		if (h < 24 && h >= 0) {
+			this.seconds = s;
+		} else {
+			this.seconds = 0;
+		}
+
+	}
+	public String getTime() {
+		return hours  + ":" + minutes + ":" + seconds;
+		
+	}
+
+	public Time() {
+
+		this.hours = this.minutes = this.seconds = 0;
+		
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
