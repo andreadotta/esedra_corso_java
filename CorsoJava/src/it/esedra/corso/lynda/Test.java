7ca1cee2a683ec9ab561d54db7afb320cc8df613
mcbra <mcbra@LAPTOP-N4LO8PAM.station>
2020-06-24 17:11:30 +0200

    test java

---
+++ CorsoJava/src/it/esedra/corso/lynda/Test.java
@@ -0,0 +1,19 @@
+package it.esedra.corso.lynda;
+public class Test {
+
+	public static void main(String args[]) {
+		int[] age = { 28, 32, 36, 40 };
+
+		for (int x : age) {
+			System.out.print(x);
+			System.out.print(",");
+		}
+		System.out.print("\n");
+		String[] names = { "Joke", "Layo", "Tola", "Busi" };
+
+		for (String name : names) {
+			System.out.print(name);
+			System.out.print(",");
+		}
+	}
+}
