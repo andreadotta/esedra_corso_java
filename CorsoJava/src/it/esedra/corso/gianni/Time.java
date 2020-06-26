709ed985332700ee3c2edf433c752f3ef7424aa1
gpaol <gpaol@LAPTOP-290V5EHF.lan>
2020-06-26 16:21:37 +0200

    corso

--- CorsoJava/src/it/esedra/corso/gianni/Time.java
+++ CorsoJava/src/it/esedra/corso/gianni/Time.java
@@ -21,6 +21,7 @@ public class Time {
 	/**
 	 * questo metodo ritorna una stringa di ore minuti secondi e millisecondi.
 	 * 
+	 * @return String orario in forma ore:minuti:secondi:millisecondi
 	 */
 	
 	public String getTime() {
@@ -30,6 +31,10 @@ public class Time {
 	/**
 	 * questo metodo setta ore minuti secondi e millisecondi ai valori che gli vengono passati controllando che siano valori corretti. 
 	 * 
+	 * @param h ore
+	 * @param m minuti
+	 * @param s secondi
+	 * @param ms millisecondi
 	 */
 	public void setTime(int h, int m, int s, int ms) {
 
