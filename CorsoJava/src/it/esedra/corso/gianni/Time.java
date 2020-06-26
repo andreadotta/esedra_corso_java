f5de36bbd6b2ce661257167c4e68933f9d2d7741
gpaol <gpaol@LAPTOP-290V5EHF.lan>
2020-06-26 15:42:30 +0200

    corso

--- CorsoJava/src/it/esedra/corso/gianni/Time.java
+++ CorsoJava/src/it/esedra/corso/gianni/Time.java
@@ -15,12 +15,22 @@ public class Time {
 	private int hours;
 	private int minutes;
 	private int seconds;
+	private int milliseconds;
+	
+	/**
+	 * questo metodo ritorna una stringa di ore minuti secondi e millisecondi.
+	 * 
+	 */
 	
 	public String getTime() {
-		return this.hours + ":" + this.minutes + ":" + this.seconds;
+		return this.hours + ":" + this.minutes + ":" + this.seconds + ":" + this.milliseconds;
 	}
 	
-	public void setTime(int h, int m, int s) {
+	/**
+	 * questo metodo setta ore minuti secondi e millisecondi ai valori che gli vengono passati controllando che siano valori corretti. 
+	 * 
+	 */
+	public void setTime(int h, int m, int s, int ms) {
 
 		if (h < 24 && h >= 0) {
 			this.hours = h;
@@ -39,11 +49,22 @@ public class Time {
 		} else {
 			this.seconds = 0;
 		}
+		
+		if (ms < 1000 && ms >= 0) {
+			this.milliseconds = ms;
+		} else {
+			this.milliseconds = 0;
+		}
 
 	}
+	
+	/**
+	 * questo costruttore setta ore minuti secondi e millisecondi a 0.
+	 * 
+	 */
 
 	public Time() {
-		this.hours = this.minutes = this.seconds = 0;
+		this.hours = this.minutes = this.seconds = this.milliseconds = 0;
 	}
 
 	public static void main(String[] args) {
