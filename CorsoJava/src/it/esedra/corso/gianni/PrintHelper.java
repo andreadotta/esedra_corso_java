5112b5372b1c432b361fe88dd1f63d4eac95219b
gpaol <gpaol@LAPTOP-290V5EHF.lan>
2020-07-01 18:31:13 +0200

    commento

--- CorsoJava/src/it/esedra/corso/gianni/PrintHelper.java
+++ CorsoJava/src/it/esedra/corso/gianni/PrintHelper.java
@@ -3,6 +3,15 @@ package it.esedra.corso.gianni;
 import it.esedra.corso.libs.*;
 
 public class PrintHelper {
+	/**
+	 * Metodo che effettua il print su system out di un messaggio + di una stringa 
+	 * e di data e ora in questa forma:
+	 * [ORE:MINUTI:SECONDI:MILLISECONDI] - MESSAGGIO - STRINGA
+	 * IL messaggio può essere NULL e quindi printa
+	 * [ORE:MINUTI:SECONDI:MILLISECONDI] - STRINGA
+	 * @param msg
+	 * @param out
+	 */
 
 	public static void out(String msg, String out) {
 
