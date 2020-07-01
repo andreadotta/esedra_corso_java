68a07bfc19de3bb5d28a1e827e4ebe73677f44cb
gpaol <gpaol@LAPTOP-290V5EHF.lan>
2020-07-01 17:21:35 +0200

    out

--- CorsoJava/src/it/esedra/corso/gianni/PrintHelper.java
+++ CorsoJava/src/it/esedra/corso/gianni/PrintHelper.java
@@ -1,29 +1,19 @@
 package it.esedra.corso.gianni;
 
-import java.util.Calendar;
-
-import java.util.Date;
-
 import it.esedra.corso.libs.*;
 
 public class PrintHelper {
 
-	public static void print() {
-		final String msg = null;
-		final String stringa = ".";
-		
-		Calendar cal = Calendar.getInstance();
-		Date now = new Date();
-		cal.setTime(now);
-		Time time = new Time();		
-		
-		time.setHours(cal.get(Calendar.HOUR_OF_DAY));
-		time.setMinutes(cal.get(Calendar.MINUTE));
-		time.setSeconds(cal.get(Calendar.SECOND));
-		time.setMilliseconds(cal.get(Calendar.MILLISECOND));
-		
-		System.out.println("[" + time.getTime() + "]" + "-" + msg + "-" + stringa);
-		
+	public static void out(String msg, String out) {
+		Time time = new Time();
+
+		if (msg == null) {
+			System.out.println("[" + time.getTime() + "]" + "-" + out);
+
+		} else {
+			System.out.println("[" + time.getTime() + "]" + "-" + msg + "-" + out);
+		}
+
 	}
 
 }
