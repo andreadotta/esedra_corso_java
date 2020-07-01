7266d5b427ade89072804c91fc64b6abc8847672
pc <pc@DESKTOP-MFS6HC4.station>
2020-07-01 18:20:38 +0200

    helper3

---
+++ CorsoJava/src/it/esedra/corso/oussein/PrintHelper.java
@@ -0,0 +1,30 @@
+package it.esedra.corso.oussein;
+
+import it.esedra.corso.libs.Time;
+
+public class PrintHelper {
+	
+	private static String info;
+
+static {
+
+		
+		info = "Current time is: ";
+		
+}
+
+	public static void out(String info,String out) {
+		
+		
+		
+		Time ora = new Time();
+
+		if (info == null) {
+			System.out.println("[" + ora.getTime() + "]" + "-" + out);
+
+		} else {
+			System.out.println("[" + ora.getTime() + "]" + "-" + info + "-" + out);
+		}
+	
+	}
+}
