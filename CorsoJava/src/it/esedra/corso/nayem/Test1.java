cfdb4f287053a24003c5a8902f48e098a4af086b
nayem <nayem@Nayem-Sarker.lan>
2020-07-03 18:49:36 +0200

    encapsulation

---
+++ CorsoJava/src/it/esedra/corso/nayem/Test1.java
@@ -0,0 +1,14 @@
+package it.esedra.corso.nayem;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		EncapsulationDemo ob = new EncapsulationDemo();
+	    ob.setName("Nayem");
+	    ob.setAge(22);
+	    System.out.println(ob.getName());
+        System.out.println(ob.getAge());
+
+	}
+
+}
