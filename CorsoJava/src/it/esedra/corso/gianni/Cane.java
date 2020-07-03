626e3c45a7b5b700e2b1bce1d53b12954dfc70dd
bauhausk <andrea@xkoll.com>
2020-07-03 19:14:18 +0200

    definizione

--- CorsoJava/src/it/esedra/corso/gianni/Cane.java
+++ CorsoJava/src/it/esedra/corso/gianni/Cane.java
@@ -6,8 +6,10 @@ import java.io.*;
 
 
 class Cane extends Animale {
-	
+	public void Cane() {
+		this.setVerso("bau");
+	}
     public void faiVerso() {
-    	System.out.println("il verso è" + getVerso());
+    	System.out.println("il verso del cane è " + getVerso());
     }
 }
