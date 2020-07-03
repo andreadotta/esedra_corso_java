736eb19eb87e44057586451e97689368a058b77d
bauhausk <andrea@xkoll.com>
2020-07-03 19:59:49 +0200

    wip

---
+++ CorsoJava/src/it/esedra/corso/oussein/DerivateClient.java
@@ -0,0 +1,10 @@
+package it.esedra.corso.oussein;
+
+public class DerivateClient {
+
+	public static void main(String args[]) {
+		
+		Derivate derivate = new Derivate();
+		derivate.setNome("Oussein");
+	}
+}
