af0d482a34f805daa68f0bf57e76b226678688e3
pc <pc@DESKTOP-MFS6HC4.station>
2020-07-03 19:44:56 +0200

    exo2

---
+++ CorsoJava/src/it/esedra/corso/oussein/Base.java
@@ -0,0 +1,24 @@
+package it.esedra.corso.oussein;
+
+public class Base {
+
+	private String nome;
+	private int salary;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+}
---
+++ CorsoJava/src/it/esedra/corso/oussein/Derivate.java
@@ -0,0 +1,13 @@
+package it.esedra.corso.oussein;
+
+public class Derivate extends Base {
+
+	public static void main(String[] args) {
+		Derivate sig = new Derivate();
+		sig.setNome("andrea");
+		sig.setSalary(5300);
+		System.out.println(sig.getNome());
+		System.out.println(sig.getSalary());
+
+	}
+}
