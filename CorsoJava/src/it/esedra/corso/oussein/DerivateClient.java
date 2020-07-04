9325ade180539c88f696e2d5d697a773e14dbe9c
pc <pc@DESKTOP-MFS6HC4.station>
2020-07-04 15:10:18 +0200

    getset

--- CorsoJava/src/it/esedra/corso/oussein/DerivateClient.java
+++ CorsoJava/src/it/esedra/corso/oussein/DerivateClient.java
@@ -3,11 +3,16 @@ package it.esedra.corso.oussein;
 public class DerivateClient {
 
 	public static void main(String args[]) {
-		
+
 		Derivate derivate = new Derivate();
 		derivate.setNome("Oussein");
-		
-		
-		derivate.print();
+		System.out.println(derivate.getNome());
+
+		derivate.setSalary(5300);
+		System.out.println(derivate.getSalary());
+
+		derivate.setCAP(51031);
+		System.out.println(derivate.getCAP());
+
 	}
 }
