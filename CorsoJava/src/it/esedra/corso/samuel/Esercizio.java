3978c4dadf0f62d9c5371b859eecd6f9a473fc2f
Samuel <Samuel@LAPTOP-R7514BMD.lan>
2020-07-06 19:30:15 +0200

    NumeroIntero esercizio!

---
+++ CorsoJava/src/it/esedra/corso/samuel/Esercizio.java
@@ -0,0 +1,27 @@
+package it.esedra.corso.samuel;
+
+public class Esercizio {
+	
+
+
+	public static void main(String[] args) {                         //segnatura//
+		
+		NumeroIntero numero1 = new NumeroIntero();                   //corpo//
+	    numero1.numeroIntero = 40;
+	    numero1.stampaNumero();
+	    
+	    
+	    
+	    NumeroIntero numero40 = new NumeroIntero ();
+	    numero40.numeroIntero = 40;
+	    numero40.stampaNumero();
+	    
+	    
+	    
+	  
+	}
+}
+	//Antonio age is 40 & Marco age is 40;
+
+    
+	
---
+++ CorsoJava/src/it/esedra/corso/samuel/NumeroIntero.java
@@ -0,0 +1,23 @@
+package it.esedra.corso.samuel;
+
+public class NumeroIntero {
+	
+	
+		public int numeroIntero;
+		public void stampaNumero()
+		{
+			System.out.println(numeroIntero);
+		}
+	
+    }
+
+
+
+	
+	
+	
+	
+	
+
+
+
