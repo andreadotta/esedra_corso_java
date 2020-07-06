e6869eeb909c2d144bd6dc04ba26249cbee9399b
marco <marco@marco-s-little-toy>
2020-07-06 13:07:01 +0200

    modifiche classe DesktopPcAssemblyAlerts

--- CorsoJava/src/it/esedra/corso/marco/DesktopPcAssemblyAlerts.java
+++ CorsoJava/src/it/esedra/corso/marco/DesktopPcAssemblyAlerts.java
@@ -1,15 +1,9 @@
 package it.esedra.corso.marco;
 
-public class DesktopPcAssemblyAlerts extends DesktopPc {
+public class DesktopPcAssemblyAlerts extends DesktopPcAssembly {
 	
 	public String getMotherboardCpuSocketType() {
 		return "Motherboard CPU Socket Type: " + this.motherboardCpuSocketType + " (!!! Check for compatibility with the CPU socket !!!)";
 	}
 	
-	public String getCoreAssembly() {
-		
-		return "PC's Core Assembly ===> " + getMotherboardFormat() + " - " + getMotherboardVendorName() + " - " + this.getMotherboardCpuSocketType() + " - " + getCpuVendorName() + " - " + getCpuClock();
-	
-	}
-
 }
