fe8fe527439ddb35df227b1da770d72f940c6573
mcbra <mcbra@LAPTOP-N4LO8PAM.station>
2020-07-06 16:44:54 +0200

    assignment

--- CorsoJava/src/it/esedra/corso/lynda/Age.java
+++ CorsoJava/src/it/esedra/corso/lynda/Age.java
@@ -1,12 +1,23 @@
 package it.esedra.corso.lynda;
 
 public class Age {
- 
+	
 	public static void main(String[] args) {
+	} {
+		  int numero1 = 40 ;
+		  int numero2 = 60 ;
+		NumeroIntero numero = new NumeroIntero (); //instance
+		numero.numeroIntero = 40; //object ("forty"); becomes a local variable
+        numero.numeroIntero = 60;
 		
-		String NumeroIntero
-		// TODO Auto-generated method stub
-
+		
+		/*** Questa classe definisce il concetto di numero intero come oggetto. In essa
+		*vengono dichiarati una variabile (ovviamente) intera ed un metodo che stamperà la variabile stessa.
+		*Scrivere, compilare ed eseguire una classe che:
+		*- istanzierà almeno due oggetti dalla classe NumeroIntero (contenente ovviamente un metodo main()), 
+		*- cambierà il valore delle relative variabili 
+         **/
+	}
 	}
 
-}
+
--- CorsoJava/src/it/esedra/corso/lynda/NumeroIntero.java
+++ CorsoJava/src/it/esedra/corso/lynda/NumeroIntero.java
@@ -1,26 +1,11 @@
 package it.esedra.corso.lynda;
-
-public class NumeroIntero {
-
-	public static void main(String[] args) {
-	
-		 System.out.println("40");
-		 System.out.println("60");
-		 
-     private int var numerointero = 100;
-     
-		 
-		 System.out.println(100);
-		
-		
+public class NumeroIntero
+{
+	public int numeroIntero;
+	public void stampaNumero()
+	{
+		System.out.println(numeroIntero);
 	}
-	
 }
 
- /**
-*Viene fornita (copiare, salvare e compilare) la seguente classe:
-*	public int numeroIntero;
-*	public void stampaNumero()
-*		System.out.println(numeroIntero);
-*	}
-/
+
