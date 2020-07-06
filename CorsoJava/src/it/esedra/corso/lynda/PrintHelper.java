c6f8f17e7a1f2a51a954ba0cd829ad5ef24244f5
bauhausk <andrea@xkoll.com>
2020-07-06 16:56:30 +0200

    correzione errore compilazione

--- CorsoJava/src/it/esedra/corso/lynda/PrintHelper.java
+++ CorsoJava/src/it/esedra/corso/lynda/PrintHelper.java
@@ -2,26 +2,17 @@ package it.esedra.corso.lynda;
 
 public class PrintHelper {
 
- String salutes = "Saanu Duniya"; {
-	 
-	
-	public static void main (String[] args) {
-		
-	System.out.print("salutes");
+	public static void main(String[] args) {
 
-		
-		//= "PrintHelper" 
-}
+		System.out.print("salutes");
 
-	
-}
+	}
 }
 //un metodo statico che effettua il print su system out di un messaggio + di una stringa 
-	// e di data e ora in questa forma:
-	
-	// [ORE:MINUTI:SECONDI:MILLISECONDI] - MESSAGGIO - STRINGA
-	
-	//IL messaggio può essere NULL e quindi printa
-	
-	// [ORE:MINUTI:SECONDI:MILLISECONDI] - STRINGA
-	
+// e di data e ora in questa forma:
+
+// [ORE:MINUTI:SECONDI:MILLISECONDI] - MESSAGGIO - STRINGA
+
+// IL messaggio può essere NULL e quindi printa
+
+// [ORE:MINUTI:SECONDI:MILLISECONDI] - STRINGA
