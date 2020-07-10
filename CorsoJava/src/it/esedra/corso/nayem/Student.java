d9995470ec20596bd8b9fbaa24eaa84e26bfd58f
nayem <nayem@Nayem-Sarker.lan>
2020-07-10 19:32:33 +0200

    ..

--- CorsoJava/src/it/esedra/corso/nayem/Student.java
+++ CorsoJava/src/it/esedra/corso/nayem/Student.java
@@ -1,11 +1,54 @@
 package it.esedra.corso.nayem;
 
-public class Student extends University {
+public class Student {
+	 private String Name;
+	 private String Surename;
+	 private int age;
+	 private University university;
 	
+	 
+
+	public University getUniversity() {
+		return university;
+	}
+
+
+
+	public void setUniversity(University university) {
+		this.university = university;
+	}
+
+
+
+	public void display() {
 	
-	 void display2() {
 			
-	 System.out.println("Student  info");
-			}
+		}
+			
+		
+		
+    public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public String getSurename() {
+		return Surename;
+	}
+
+	public void setSurename(String surename) {
+		Surename = surename;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
    
 }
