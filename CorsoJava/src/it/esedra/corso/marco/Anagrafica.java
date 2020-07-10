82044bf6f8163e3b4f5b671399f2fba431068ed4
bauhausk <andrea@xkoll.com>
2020-07-10 19:18:19 +0200

    anagrfica

--- CorsoJava/src/it/esedra/corso/marco/Anagrafica.java
+++ CorsoJava/src/it/esedra/corso/marco/Anagrafica.java
@@ -2,4 +2,13 @@ package it.esedra.corso.marco;
 
 public class Anagrafica {
 
+	private Persona persona;
+
+	public Persona getPersona() {
+		return persona;
+	}
+
+	public void setPersona(Persona persona) {
+		this.persona = persona;
+	}
 }
