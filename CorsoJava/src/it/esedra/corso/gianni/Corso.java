367f5d30c53fedc4f8daf47e3a86c5b3a4626a0d
bauhausk <andrea@xkoll.com>
2020-07-13 17:25:02 +0200

    merge

--- CorsoJava/src/it/esedra/corso/gianni/Corso.java
+++ CorsoJava/src/it/esedra/corso/gianni/Corso.java
@@ -2,15 +2,14 @@ package it.esedra.corso.gianni;
 
 public class Corso {
 	public static void main(String[] args) {
-		Poligono rettangolo = null;//new Rettangolo(5, 10);
 
+		Poligono rettangolo = null;//new Rettangolo(5, 10);
 
 		rettangolo.calcolaArea();
 		rettangolo.calcolaPerimetro();
 
 		PrintHelper.out("Area del rettangolo", rettangolo.getArea().toString());
 		PrintHelper.out("Perimentro del rettangolo", rettangolo.getPerimetro().toString());
-		
 
 
 	}
