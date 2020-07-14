e07196779e92ac799236ed392fb28a97f77003a1
pc <pc@DESKTOP-MFS6HC4.station>
2020-07-14 18:25:19 +0200

    daointerface

--- CorsoJava/src/it/esedra/corso/journal/Author.java
+++ CorsoJava/src/it/esedra/corso/journal/Author.java
@@ -2,6 +2,7 @@ package it.esedra.corso.journal;
 
 /**
  * Questa classe è un autore del capitolo
+ * 
  * @author bauhausk
  *
  */
@@ -9,9 +10,37 @@ public class Author {
 
 	private String name;
 	private String email;
-	
-	public Author() {
-		// TODO Auto-generated constructor stub
+	private String src;
+
+	public Author(String name, String email, String src) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.src = src;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSrc() {
+		return src;
+	}
+
+	public void setSrc(String src) {
+		this.src = src;
 	}
 
 }
