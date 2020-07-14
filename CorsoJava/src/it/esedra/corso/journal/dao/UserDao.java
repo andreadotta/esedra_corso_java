3d9d5c2ddbfa010e267268fde0aebd73ad5d8ca1
mcbra <mcbra@LAPTOP-N4LO8PAM.station>
2020-07-14 19:26:44 +0200

    userDao sl

---
+++ CorsoJava/src/it/esedra/corso/journal/dao/UserDao.java
@@ -0,0 +1,30 @@
+package it.esedra.corso.journal.dao;
+
+import it.esedra.corso.journal.User;
+
+public class UserDao implements DaoInterface {
+	private User user;
+
+	public UserDao(User user) {
+		super();
+		this.user = user;
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void get() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+}
