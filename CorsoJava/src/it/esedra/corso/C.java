5bd1ff7a6cc394a65e8ec93f13829a5dcacbca36
bauhausk <andrea@xkoll.com>
2020-07-16 18:05:20 +0200

    generics

---
+++ CorsoJava/src/it/esedra/corso/C.java
@@ -0,0 +1,16 @@
+package it.esedra.corso;
+
+public class C<T> { // T è un type paramenter
+
+	private T c;
+
+	public T getC() {
+		return c;
+	}
+
+	public void setC(T c) {
+		this.c = c;
+	}
+
+	
+}
