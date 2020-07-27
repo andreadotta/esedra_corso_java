6e9497c0c413e8f6287559272a9f82a0c19d6f10
gpaol <gpaol@LAPTOP-290V5EHF.lan>
2020-07-27 18:29:18 +0200

    sort

---
+++ CorsoJava/src/it/esedra/corso/gianni/Main.java
@@ -0,0 +1,13 @@
+package it.esedra.corso.gianni;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		float[] a = {1, 8, 5, 6, 9, 15, 2};
+		float b[] = Sort.sortArray(a);
+		for (int i = 0; i < b.length; i++) {
+			System.out.println(b[i]);
+
+		}
+	}
+}
