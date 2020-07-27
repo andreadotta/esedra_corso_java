bdc6b971b9244caba11aa5ff868bbf315bbd92a1
bauhausk <andrea@xkoll.com>
2020-07-27 17:02:10 +0200

    remerge

---
+++ CorsoJava/src/it/esedra/corso/journal/Chapter.java
@@ -0,0 +1,54 @@
+package it.esedra.corso.journal;
+
+/**
+ * Questa classe è un capitolo del diario
+ * 
+ * @author bauhausk
+ * 
+ */
+public class Chapter {
+
+	private String nome;
+	private String congnome;
+	private String citta;
+	private String professione;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCongnome() {
+		return congnome;
+	}
+
+	public void setCongnome(String congnome) {
+		this.congnome = congnome;
+	}
+
+	public String getCitta() {
+		return citta;
+	}
+
+	public void setCitta(String citta) {
+		this.citta = citta;
+	}
+
+	public String getProfessione() {
+		return professione;
+	}
+
+	public void setProfessione(String professione) {
+		this.professione = professione;
+	}
+
+	
+
+	public Chapter() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- CorsoJava/src/it/esedra/corso/journal/Journal.java
+++ CorsoJava/src/it/esedra/corso/journal/Journal.java
@@ -1,4 +1,3 @@
-
 package it.esedra.corso.journal;
 
 /**
@@ -19,25 +18,3 @@ public class Journal {
 	}
 
 }
-=======
-package it.esedra.corso.journal;
-
-/**
- * Classe base dell'applicazione
- *  
- * @author bauhausk
- *
- */
-public class Journal {
-
-	public static final String DBPATH = System.getProperty("user.dir") + "/sqlite/db/journal.sql";
-	private Chapter[] chapters;
-
-	private Author author;
-	
-	public Journal() {
-
-	}
-}
-
->>>>>>> branch 'master' of https://github.com/andreadotta/esedra_corso_java.git
