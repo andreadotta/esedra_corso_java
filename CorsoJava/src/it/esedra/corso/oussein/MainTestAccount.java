864220ca8ec4c1378ed712d7d44a6bca73b9f5db
pc <pc@DESKTOP-MFS6HC4>
2020-08-10 15:25:38 +0200

    genericis

---
+++ CorsoJava/src/it/esedra/corso/oussein/MainTestAccount.java
@@ -0,0 +1,23 @@
+package it.esedra.corso.oussein;
+
+public class MainTestAccount {
+
+	public static void main(String[] args) {
+		Account<Character> myAccount=new Account<>("Ousseynou",200, 'e'  );
+		myAccount.showBalance();
+		
+		Account<String> otherAccount=new Account<>("Ndiemane",100, "euros");
+		otherAccount.showBalance();
+		
+		Bank bank =new Bank("WeusBank");
+		bank.transfer(myAccount, otherAccount,25);
+		
+		myAccount.showBalance();
+		otherAccount.showBalance();
+
+	}
+
+}
+
+
+
