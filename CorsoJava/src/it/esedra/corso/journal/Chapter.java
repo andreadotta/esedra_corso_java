8197b55ce7830e42bd1c3371a943f3664d18b54f
bauhausk <andrea@xkoll.com>
2020-08-24 19:25:47 +0200

    Test case

--- CorsoJava/src/it/esedra/corso/journal/Chapter.java
+++ CorsoJava/src/it/esedra/corso/journal/Chapter.java
@@ -1,5 +1,7 @@
 package it.esedra.corso.journal;
 
+import java.util.Date;
+
 /**
  * Questa classe è un capitolo del diario
  * 
@@ -7,12 +9,40 @@ package it.esedra.corso.journal;
  * 
  */
 public class Chapter {
+
+	private String title;
+
+	private Date date;
+
+	private String[] paragraph;
+
 	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String[] getParagraph() {
+		return paragraph;
+	}
+
+	public void setParagraph(String[] paragraph) {
+		this.paragraph = paragraph;
+	}
 
 	public Chapter() {
 		// TODO Auto-generated constructor stub
 	}
 
-	
-	
 }
