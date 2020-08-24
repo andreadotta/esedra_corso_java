c576c85b37eacca6946d1136b8195d70e2db585d
bauhausk <andrea@xkoll.com>
2020-08-24 19:00:25 +0200

    refactoring journal

--- CorsoJava/src/it/esedra/corso/journal/Chapter.java
+++ CorsoJava/src/it/esedra/corso/journal/Chapter.java
@@ -7,44 +7,6 @@ package it.esedra.corso.journal;
  * 
  */
 public class Chapter {
-
-	private String nome;
-	private String congnome;
-	private String citta;
-	private String professione;
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getCongnome() {
-		return congnome;
-	}
-
-	public void setCongnome(String congnome) {
-		this.congnome = congnome;
-	}
-
-	public String getCitta() {
-		return citta;
-	}
-
-	public void setCitta(String citta) {
-		this.citta = citta;
-	}
-
-	public String getProfessione() {
-		return professione;
-	}
-
-	public void setProfessione(String professione) {
-		this.professione = professione;
-	}
-
 	
 
 	public Chapter() {
