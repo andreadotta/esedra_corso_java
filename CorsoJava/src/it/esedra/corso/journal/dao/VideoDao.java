5cc2bfe6febf32b94fbc8af0fe661a5076f6f81f
innig <innig@innig-PC.lan>
2020-08-26 17:32:06 +0200

    videodao

--- CorsoJava/src/it/esedra/corso/journal/dao/VideoDao.java
+++ CorsoJava/src/it/esedra/corso/journal/dao/VideoDao.java
@@ -5,32 +5,49 @@ import java.sql.Connection;
 import it.esedra.corso.collections.interfaces.Collection;
 import it.esedra.corso.journal.Video;
 
-public class VideoDao implements DaoInterface<Video> { 
+public class VideoDao implements DaoInterface<Video> {
 
 	private Video video;
 	private Connection conn;
-	
+
 	public VideoDao(Video video) {
 		super();
 		this.video = video;
 	}
 
-    @Override
+	@Override
 	public void update() {
-		
+
 	}
 
 	@Override
 	public void delete() {
-		
 
 	}
 
 	@Override
 	public Collection<Video> getAll() {
-		
-		return null;
-		
+		Collection<Video> videos = new VideoCollection();
+		try {
+			Statement stm = this.conn.createStatement();
+			ResultSet rs = stm.executeQuery("SELECT * FROM video");
+
+			while (rs.next()) {
+
+				Video video = new Video();
+				video.setVideoTitle(rs.getString("Title"));
+				video.setVideoID(rs.getString("ID"));
+				video.setVideoURL(rs.getString("URL"));
+				videos.add(video);
+
+			}
+			rs.close();
+		} catch (Exception e) {
+			PrintHelper.out("Errore video dao", e.getMessage());
+		}
+
+		return videos;
+
 	}
 
 	@Override
