f9bd15113b21c4fd042f7d59e9b3473a64ff49a6
bauhausk <andrea@xkoll.com>
2020-08-26 18:32:55 +0200

    import collection

--- CorsoJava/src/it/esedra/corso/collections/DoubleCollection.java
+++ CorsoJava/src/it/esedra/corso/collections/DoubleCollection.java
@@ -2,7 +2,6 @@ package it.esedra.corso.collections;
 
 import it.esedra.corso.collections.interfaces.Collection;
 import it.esedra.corso.collections.interfaces.Iterator;
-import it.esedra.corso.helpers.PrintHelper;
 
 public class DoubleCollection extends AbstactCollections<Double> implements Collection<Double> {
 
