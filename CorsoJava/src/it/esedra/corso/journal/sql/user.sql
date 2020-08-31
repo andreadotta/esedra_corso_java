220d3d85853c9f507f0e8b1a45203f4e2fdbc3b8
jesoobamboo <jesoobamboo@jesoobamboo>
2020-08-31 17:56:56 +0200

    creazione user.sql

---
+++ CorsoJava/src/it/esedra/corso/journal/sql/user.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "user" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"name"	TEXT NOT NULL,
+	"surname"	TEXT NOT NULL,
+	"email"	TEXT NOT NULL,
+	"password"	TEXT NOT NULL,
+	"registration"	TEXT NOT NULL,
+	PRIMARY KEY("userId" AUTOINCREMENT)
+)
