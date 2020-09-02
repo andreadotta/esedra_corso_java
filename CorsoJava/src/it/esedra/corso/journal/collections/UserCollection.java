90ec79968bf2f28d815a18c2a9124374db6d8fe3
JesooBamboo <JesooBamboo@DESKTOP-MNVTR76.station>
2020-09-02 15:33:54 +0200

    rimozione import e del marco.sqbpro

--- CorsoJava/src/it/esedra/corso/journal/collections/UserCollection.java
+++ CorsoJava/src/it/esedra/corso/journal/collections/UserCollection.java
@@ -1,11 +1,8 @@
 package it.esedra.corso.journal.collections;
 
-import java.util.Arrays;
-
 import it.esedra.corso.collections.AbstactCollections;
 import it.esedra.corso.collections.Iterator;
 import it.esedra.corso.collections.interfaces.Collection;
-import it.esedra.corso.helpers.PrintHelper;
 import it.esedra.corso.journal.User;
 
 public class UserCollection extends AbstactCollections<User> implements Collection<User> {
