dc7bec62a6aecc3fd71f697bdde8c0cd36935eb3
pc <pc@DESKTOP-MFS6HC4.station>
2020-09-02 18:53:48 +0200

    para

--- CorsoJava/src/it/esedra/corso/journal/test/ParagraphTest.java
+++ CorsoJava/src/it/esedra/corso/journal/test/ParagraphTest.java
@@ -3,12 +3,13 @@ package it.esedra.corso.journal.test;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import it.esedra.corso.collections.ParagraphCollection;
+
 import it.esedra.corso.collections.interfaces.Collection;
 import it.esedra.corso.collections.interfaces.Iterator;
 import it.esedra.corso.db.DbConnect;
 import it.esedra.corso.helpers.PrintHelper;
 import it.esedra.corso.journal.Paragraph;
+import it.esedra.corso.journal.collections.ParagraphCollection;
 import it.esedra.corso.journal.dao.ParagraphDao;
 
 public class ParagraphTest {
