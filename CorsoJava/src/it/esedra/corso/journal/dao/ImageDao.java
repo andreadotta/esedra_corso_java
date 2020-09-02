d8731efc32f8dee2651d2d3909c089b14222e0ff
nayem <nayem@Nayem-Sarker.lan>
2020-09-02 18:58:36 +0200

    image

--- CorsoJava/src/it/esedra/corso/journal/dao/ImageDao.java
+++ CorsoJava/src/it/esedra/corso/journal/dao/ImageDao.java
@@ -2,9 +2,13 @@ package it.esedra.corso.journal.dao;
 
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import it.esedra.corso.collections.interfaces.Collection;
+import it.esedra.corso.helpers.PrintHelper;
 import it.esedra.corso.journal.Image;
+import it.esedra.corso.journal.Video;
 import it.esedra.corso.journal.collections.ImageCollection;
 
 public class ImageDao implements DaoInterface<Image> {
@@ -31,9 +35,43 @@ public class ImageDao implements DaoInterface<Image> {
 	@Override
 	public Collection<Image> getAll() {
 		Collection<Image> images = new ImageCollection();
+		try {
+			Statement stm = this.conn.createStatement();
+			ResultSet rs = stm.executeQuery("SELECT * FROM image");
+
+			while (rs.next()) {
+
+				Image image = new Image();
+				image.setId(rs.getInt("id"));
+				image.setSrc(rs.getString("src"));
+				images.add(image);
+
+			}
+			rs.close();
+		} catch (Exception e) {
+			PrintHelper.out("Errore video dao", e.getMessage());
+		}
+
+		return Image;
+
 		
-		return null;
 		
+	}
+
+	public Image getImage() {
+		return image;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
+
+	public Connection getConn() {
+		return conn;
+	}
+
+	public void setConn(Connection conn) {
+		this.conn = conn;
 	}
 
 	@Override
