e68138632366e2e19731c3234d278d4911929642
JesooBamboo <JesooBamboo@DESKTOP-MNVTR76.station>
2020-09-02 23:34:01 +0200

    aggiunta metodo update e modifica userTest

--- CorsoJava/src/it/esedra/corso/journal/test/UserTest.java
+++ CorsoJava/src/it/esedra/corso/journal/test/UserTest.java
@@ -10,17 +10,41 @@ import it.esedra.corso.helpers.PrintHelper;
 import it.esedra.corso.journal.User;
 import it.esedra.corso.journal.collections.UserCollection;
 import it.esedra.corso.journal.dao.UserDao;
+import it.esedra.corso.journal.db.JournalDbConnect;
 
 public class UserTest {
+
+	// Parametri database
+	public static final String DBPATH = System.getProperty("user.dir") + "/sqlite/db/marco.db";
 
 	public UserTest() {
 
 	}
 
-	// Parametri database
-	public static final String DBPATH = System.getProperty("user.dir") + "/sqlite/db/marco.db";
+	public void testUpdate() {
 
-	public static void main(String[] args) {
+		try {
+			Connection connection = JournalDbConnect.connect();
+
+			User user = new User();
+			user.setId(1);
+			user.setName("Claudio");
+			user.setSurname("Cini");
+			user.setEmail("ccini@foo.com");
+			user.setPassword("bge");
+			user.setRegistration("01/06/2018");
+
+			UserDao userDao = new UserDao(user);
+			userDao.setConnection(connection);
+			userDao.update();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void testGetAll() {
 
 		Collection<User> userCollection = new UserCollection();
 
@@ -29,17 +53,18 @@ public class UserTest {
 			Connection connection = DbConnect.connect(DBPATH);
 			UserDao userdao = new UserDao(new User());
 			userdao.setConnection(connection);
-						
+
 			// Chiamata metodo getAll() sulla Collection creata
 			userCollection = userdao.getAll();
-			
+
 			// Inizializzazione iterator per ciclare sulla Collection
 			Iterator<User> userIterator = userCollection.createIterator();
-			
+
 			// cicla sugli elementi User della userCollection e restituisce per ogni
 			// elemento i valori delle colonne della tabella user
 			while (userIterator.hasNext()) {
 				User user = userIterator.next();
+				PrintHelper.out("User ID: " + user.getId());
 				PrintHelper.out("Nome e cognome: " + user.getName() + " " + user.getSurname());
 				PrintHelper.out("Email: " + user.getEmail());
 				PrintHelper.out("Password: " + user.getPassword());
@@ -51,6 +76,14 @@ public class UserTest {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+
+	}
+
+	public static void main(String[] args) {
+
+		UserTest ut = new UserTest();
+		ut.testUpdate();
+		ut.testGetAll();
 
 	}
 
