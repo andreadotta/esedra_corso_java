d9dedda99372155c643e698b7b08a584520a9724
mcbra <mcbra@LAPTOP-N4LO8PAM.station>
2020-09-11 18:53:37 +0200

    AUT

---
+++ CorsoJava/src/it/esedra/corso/journal/test/AuthorTest.java
@@ -0,0 +1,14 @@
+package it.esedra.corso.journal.test;
+
+public final class AuthorTest {
+
+	public AuthorTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
