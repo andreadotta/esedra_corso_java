0b0d939bcc97dbb27c238e98c4c77c12f36fd90c
JesooBamboo <JesooBamboo@DESKTOP-VR126DR.station>
2020-09-11 12:05:27 +0200

    format codice user

--- CorsoJava/src/it/esedra/corso/journal/test/UserTest.java
+++ CorsoJava/src/it/esedra/corso/journal/test/UserTest.java
@@ -1,21 +1,29 @@
 package it.esedra.corso.journal.test;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 import it.esedra.corso.collections.interfaces.Collection;
 import it.esedra.corso.collections.interfaces.Iterator;
-import it.esedra.corso.db.DbConnect;
 import it.esedra.corso.helpers.PrintHelper;
 import it.esedra.corso.journal.User;
 import it.esedra.corso.journal.collections.UserCollection;
 import it.esedra.corso.journal.dao.UserDao;
+import it.esedra.corso.journal.db.DbUtil;
 import it.esedra.corso.journal.db.JournalDbConnect;
 
 public class UserTest {
 
 	// Parametri database
-	public static final String DBPATH = System.getProperty("user.dir") + "/sqlite/db/marco.db";
+	public static final String DBPATH = System.getProperty("user.dir") + "/sqlite/db/journal.db";
+	// Dichiarazione delle costanti di classe per il test case
+	public static final int ID = 1;
+	public static final String NAME = "Claudio";
+	public static final String SURNAME = "Cini";
+	public static final String EMAIL = "ccini@foo.com";
+	public static final String PASSWORD = "bge";
+	public static final String REGISTRATION = "01/06/2018";
 
 	public UserTest() {
 
@@ -27,12 +35,12 @@ public class UserTest {
 			Connection connection = JournalDbConnect.connect();
 
 			User user = new User();
-			user.setId(1);
-			user.setName("Claudio");
-			user.setSurname("Cini");
-			user.setEmail("ccini@foo.com");
-			user.setPassword("bge");
-			user.setRegistration("01/06/2018");
+			user.setId(ID);
+			user.setName(NAME);
+			user.setSurname(SURNAME);
+			user.setEmail(EMAIL);
+			user.setPassword(PASSWORD);
+			user.setRegistration(REGISTRATION);
 
 			UserDao userDao = new UserDao(user);
 			userDao.setConnection(connection);
@@ -60,28 +68,37 @@ public class UserTest {
 			// Inizializzazione iterator per ciclare sulla Collection
 			Iterator<User> userIterator = userCollection.createIterator();
 
-			// cicla sugli elementi User della userCollection e restituisce per ogni
-			// elemento i valori delle colonne della tabella user
-			while (userIterator.hasNext()) {
-				User user = userIterator.next();
-				PrintHelper.out("User ID: " + user.getId());
-				PrintHelper.out("Nome e cognome: " + user.getName() + " " + user.getSurname());
-				PrintHelper.out("Email: " + user.getEmail());
-				PrintHelper.out("Password: " + user.getPassword());
-				PrintHelper.out("Registration Date: " + user.getRegistration());
-				PrintHelper.out("*****************************");
+			/**
+			 * Cicla sugli elementi User della userCollection e restituisce per ogni
+			 * elemento i valori delle colonne della tabella user.
+			 * 
+			 * L'if controlla l'effettiva esistenza dei dati del test case nelle colonne
+			 * della tabella user.
+			 * 
+			 */
 
-				if (user.getName() == "Claudio" && user.getSurname() == "Cini" && 
-						user.getEmail() == "ccini@foo.com" && user.getPassword() == "bge" && 
-						user.getRegistration() == "01/06/2018") {
-					PrintHelper.out("I dati coincidono.");
-				} else {
-					PrintHelper.out("Errore output. I dati non coincidono.");
+			boolean found = false;
+			while (userIterator.hasNext()) {
+
+				User user = userIterator.next();
+
+				if (user.getId() == ID && user.getName().equals(NAME) && user.getSurname().equals(SURNAME)
+						&& user.getEmail().equals(EMAIL) && user.getPassword().equals(PASSWORD)
+						&& user.getRegistration().equals(REGISTRATION)) {
+					found = true;
+					break;
 				}
 
 			}
 
 			connection.close();
+
+			if (found == true) {
+				PrintHelper.out(JournalTest.TEST_OK);
+			} else {
+				PrintHelper.out(JournalTest.TEST_FAIL);
+			}
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -90,9 +107,14 @@ public class UserTest {
 
 	public static void main(String[] args) {
 
-		UserTest ut = new UserTest();
-		ut.testUpdate();
-		ut.testGetAll();
+		try {
+			DbUtil.rebuildDb();
+			UserTest ut = new UserTest();
+			ut.testUpdate();
+			ut.testGetAll();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
