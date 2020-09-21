3d46cccc641cf27bebb003ea1edfe41fdc8bad8b
pc <pc@DESKTOP-MFS6HC4.station>
2020-09-21 13:10:47 +0200

    video cons

--- CorsoJava/src/it/esedra/corso/journal/Video.java
+++ CorsoJava/src/it/esedra/corso/journal/Video.java
@@ -6,8 +6,48 @@ package it.esedra.corso.journal;
  *
  */
 public class Video extends MultimediaResource {
+private int id;
+private String title;
+private String name;
+private String src;
 
-	private String title;
+	public int getId() {
+	return id;
+}
+
+
+
+public void setId(int id) {
+	this.id = id;
+}
+
+
+
+public String getName() {
+	return name;
+}
+
+
+
+public void setName(String name) {
+	this.name = name;
+}
+
+
+
+public String getSrc() {
+	return src;
+}
+
+
+
+public void setSrc(String src) {
+	this.src = src;
+}
+
+
+
+	
 	
 	
 	public String getTitle() {
@@ -21,7 +61,8 @@ public class Video extends MultimediaResource {
 	}
 
 
+public Video() {
 
 
-
+}
 }
