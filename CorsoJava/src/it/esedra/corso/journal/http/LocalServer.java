d6c4c38cf5099e3772c19ffc2625c40d7e7991db
bauhausk <andrea@xkoll.com>
2020-09-25 19:35:16 +0200

    author

--- CorsoJava/src/it/esedra/corso/journal/http/LocalServer.java
+++ CorsoJava/src/it/esedra/corso/journal/http/LocalServer.java
@@ -11,25 +11,35 @@ import com.sun.net.httpserver.HttpServer;
 public class LocalServer {
 
 	public static void main(String[] args) throws Exception {
-		
+
 		new LocalServer();
 	}
+
 	public LocalServer() throws IOException {
 		HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/test", new MyHandler());
-        server.setExecutor(null); // creates a default executor
-        server.start();
-    }
+		server.createContext("/author", new MyHandler());
+		server.setExecutor(null); // creates a default executor
+		server.start();
+	}
 
-    static class MyHandler implements HttpHandler {
-        @Override
-        public void handle(HttpExchange t) throws IOException {
-            String response = "This is the response";
-            t.sendResponseHeaders(200, response.length());
-            OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        }
-    }
+	static class MyHandler implements HttpHandler {
+		@Override
+		public void handle(HttpExchange t) throws IOException {
+
+			if (t.getRequestMethod().equals("POST")) {
+				
+			}
+			
+			String response = "This is the response";
+			t.sendResponseHeaders(200, response.length());
+			OutputStream os = t.getResponseBody();
+			os.write(response.getBytes());
+			os.close();
+		}
+
+		private String handleGetRequest(HttpExchange httpExchange) {
+			return httpExchange.getRequestURI().toString().split("\\?")[1].split("=")[1];
+		}
+	}
 
 }
