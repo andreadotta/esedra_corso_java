32833f7d165c0d40d98529fb7f13bbe7d2f6500b
bauhausk <andrea@xkoll.com>
2020-09-30 19:23:55 +0200

    Author.js script per gestire i campi del form author

--- CorsoJava/web/journal/js/author.js
+++ CorsoJava/web/journal/js/author.js
@@ -5,13 +5,19 @@ document.addEventListener("DOMContentLoaded", function() {
 	});
 	
 });
+/**
+ * Effettua il submit del form
+ */
 function submitAuthor(event) {
-	//impdisco il submit del form
+	//impedisco il submit del form
 	event.preventDefault();
-	
+	//creo un oggetto che rappresenta il form e ottendo i campi del form
+	//sotto forma di array
 	let formAuthor = document.getElementById("author-form").elements;
+	//ottengo i campi usando il valore dell'attributo NAME del tag INPUT come indice dell'array
 	let name = formAuthor["name"];
 	let email = formAuthor["email"];
+	//richiamo il metodo validate il quale valida i campi (controlla che siano presenti quelli obbligatori e il loro tipo sia quello atteso)
 	if (!validate(formAuthor)) {
 		return;
 	}
@@ -20,10 +26,14 @@ function submitAuthor(event) {
     console.log(email.value);
 
 }
+/**
+* Valida il form
+* Controlla che siano presenti quelli obbligatori e il loro tipo sia quello atteso 
+*/
 function validate(formAuthor) {
 	let name = formAuthor["name"];
 	let email = formAuthor["email"];
-	
+	//verifica che i campi abbamo un valore
 	if (name.value == '' || email.value == '') {
 		alert("I campi nome e email devono essere presenti");
 		return false;		
