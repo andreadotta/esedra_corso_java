748fd2bef45626b7d7e2ea4b7717f105a243c2de
Samuel <Samuel@LAPTOP-R7514BMD.lan>
2020-09-30 18:44:39 +0200

    test

--- CorsoJava/src/it/esedra/corso/journal/Journal.java
+++ CorsoJava/src/it/esedra/corso/journal/Journal.java
@@ -10,50 +10,31 @@ import java.util.Properties;
 /**
  * Classe base dell'applicazione
  * 
- * @author bauhausk
+ * @journal bauhausk
  *
  */
 public class Journal {
 	
-	private int id;
-	private String name;
-	
-	public int getId() {
-		return id;
-	}
+	private final int id;
+	private final String name;
 
-	public void setId(int id) {
+	public Journal(Integer id, String name) {
+		super();
 		this.id = id;
+		this.name = name;
+	
+
 	}
+   public int getId() {
+	   return  id ;
+   }
 
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Journal() {
-
-	}
-	/**
-	 * Carico le configurazioni dell'app presenti sul file di properties
-	 * @return Config
-	 * @throws IOException
-	 */
-	public static Config loadProperties() throws IOException {
-			
-		Properties prop = new Properties();
-		prop.load(new FileInputStream("config.properties"));
+	
 		
-		Config config = new Config();
-		config.setDbpath(prop.getProperty("dbpath"));
-
-		return config;
-		
-	}
 	
 	
-
 }
