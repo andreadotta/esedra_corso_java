7f4b2a410d6be3b05a0f40014ca6411ccb8043b1
bauhausk <andrea@xkoll.com>
2020-11-02 16:58:34 +0100

    Local server

---
+++ CorsoJava/web/journal/js/index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+	
+	document.querySelector('#button-test-server').addEventListener('click', function(event){
+		testServer();
+	});
+	
+	
+});
+	
