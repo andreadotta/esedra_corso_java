aa5ab41249be7445e5b8e93577f9d816f003e346
bauhausk <andrea@xkoll.com>
2020-11-09 18:09:14 +0100

    casting js

--- CorsoJava/src/it/esedra/corso/journal/http/RemoteHttpServer.java
+++ CorsoJava/src/it/esedra/corso/journal/http/RemoteHttpServer.java
@@ -42,73 +42,7 @@ public class RemoteHttpServer {
 		server.start();
 	}
 
-	static class MyHandler implements HttpHandler {
-		@Override
-		public void handle(HttpExchange t) throws IOException {
-
-			switch (t.getRequestMethod()) {
-			case "GET": {
-				this.handleGetRequest(t);
-				response(t);
-			}
-			case "POST": {
-				this.handlePostRequest(t);
-				response(t);
-			}
-			case "PUT": {
-				this.handlePutRequest(t);
-				response(t);
-			}
-			case "DELETE": {
-				this.handleDeleteRequest(t);
-				response(t);
-			}
-			default:
-				responseFail(t);
-			}
-
-		}
-
-		private void responseFail(HttpExchange t) throws IOException {
-			String response = "Internal Error";
-			t.sendResponseHeaders(500, response.length());
-			OutputStream os = t.getResponseBody();
-			os.write(response.getBytes());
-			os.close();
-		}
-
-		private void response(HttpExchange t) throws IOException {
-			String response = "";
-			t.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
-			t.getResponseHeaders().add("Access-Control-Allow-Headers", "*");
-			t.getResponseHeaders().add("Content-type", "plain/text");
-			t.sendResponseHeaders(200, response.length());
-			OutputStream os = t.getResponseBody();
-			os.write(response.getBytes());
-			os.close();
-		}
-
-		private void handleGetRequest(HttpExchange httpExchange) {
-			// String queryString =
-			// httpExchange.getRequestURI().toString().split("\\?")[1].split("=")[1];
-		}
-
-		private void handlePostRequest(HttpExchange httpExchange) throws IOException {
-			// return httpExchange.getRequestURI().toString().split("\\?")[1].split("=")[1];Print
-			
-			
-			   			
-			
-		}
-
-		private void handlePutRequest(HttpExchange httpExchange) {
-			// return httpExchange.getRequestURI().toString().split("\\?")[1].split("=")[1];
-		}
-
-		private void handleDeleteRequest(HttpExchange httpExchange) {
-			// return httpExchange.getRequestURI().toString().split("\\?")[1].split("=")[1];
-		}
-	}
+	
 
 	private static String streamToString(InputStream inputStream) {
 		String text = new Scanner(inputStream, "UTF-8").useDelimiter("\\Z").next();
