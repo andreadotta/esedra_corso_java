fff2d82fa47cf3646f693b8960099c895c18e845
gpaol <gpaol@LAPTOP-ORJ23J7O.lan>
2020-11-27 17:28:07 +0100

    video

--- CorsoJava/sqlite/db/sql/video.sql
+++ CorsoJava/sqlite/db/sql/video.sql
@@ -4,5 +4,7 @@ CREATE TABLE "video" (
 	"id"	INTEGER NOT NULL UNIQUE,
 	"src"	TEXT NOT NULL, 
 	"name"	TEXT NULL, 	
+	"id_journal" INTEGER NOT NULL,
 	PRIMARY KEY("id" AUTOINCREMENT)
+	FOREIGN KEY("id_journal") REFERENCES journal("id")
 );
