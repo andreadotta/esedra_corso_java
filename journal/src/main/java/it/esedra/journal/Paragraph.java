73197e8efdbec61d06abefe09768b8986f8b2a18
marcosimi <simi.marco.82@gmail.com>
2020-12-22 11:44:46 +0100

    format code Paragraph

--- journal/src/main/java/it/esedra/journal/Paragraph.java
+++ journal/src/main/java/it/esedra/journal/Paragraph.java
@@ -1,9 +1,5 @@
 package it.esedra.journal;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
-
 import it.esedra.corso.collections.interfaces.DataObjectInterface;
 
 /**
