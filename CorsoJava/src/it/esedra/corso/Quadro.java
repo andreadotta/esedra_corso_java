27af47359d6e2bce5eb55363943d60a98340318e
marcosimi <simi.marco.82@gmail.com>
2020-12-28 19:55:44 +0100

    lezione 28 12

--- CorsoJava/src/it/esedra/corso/Quadro.java
+++ CorsoJava/src/it/esedra/corso/Quadro.java
@@ -7,9 +7,64 @@ package it.esedra.corso;
  * @author bauhausk
  *
  */
-public class Quadro {
+enum Colors {
+	RED,
+	BLUE,
+	BLACK
+}
 
+public class Quadro {
+	
+	private String author;
+	private int code;
+	private Colors color;
+	
 	public Quadro() {
 
 	}
+	
+	public Quadro(String author, int code, Colors color) {
+		this.author = author;
+		this.code = code;
+		this.color = color;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public Colors getColor() {
+		return color;
+	}
+
+	public void setColor(Colors color) {
+		this.color = color;
+	}
+	
+	public String toString() {
+		return getAuthor();
+	}
+	
+	public boolean equals(Object obj) {
+		return ((Quadro) obj).getAuthor().equals(this.getAuthor());
+	
+	}
+	
+	public int hashCode() {
+		return author.hashCode();
+	}
+	
+	
 }
