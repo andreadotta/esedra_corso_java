895c70f1b6813b5c58c2e2cd7baee2692eb70168
marcosimi <simi.marco.82@gmail.com>
2021-01-04 19:50:13 +0100

    FETCH js su Author

--- CorsoJava/web/journal/js/author.js
+++ CorsoJava/web/journal/js/author.js
@@ -27,17 +27,44 @@ function createAuthor() {
 		var req = new XMLHttpRequest();
 		req.onload = function() {
 			res = JSON.parse(this.responseText);
-			if (res.status === "ok") { //il backend ha risposto positivamente; quindi procedo ad aggiornare la pagina (DOM)
-				document.getElementById("xhr-message").innerHTML = "Salvataggio riuscito"; //imposto un messaggio di successo
-				//vado a cercare una riga che abbia data-id con valore uguale al valore del campo "id" dell'oggetto 
-				//restituito dal backend
-				let foundRow = document.querySelector("[data-id='" + res.data.id + "']"); //utilizzo query selector: mi rende il primo elemento trovato
-				if (foundRow) { //se trovo un elemento con data-id
-					foundRow.innerHTML = ""; //pulisco la riga che ho trovato...
-					createRowElements(res.data, foundRow); //...e poi vado a inserire il contenuto
+			if (res.status === "ok") { // il backend ha risposto positivamente;
+										// quindi procedo ad aggiornare la
+										// pagina (DOM)
+				document.getElementById("xhr-message").innerHTML = "Salvataggio riuscito"; // imposto
+																							// un
+																							// messaggio
+																							// di
+																							// successo
+				// vado a cercare una riga che abbia data-id con valore uguale
+				// al valore del campo "id" dell'oggetto
+				// restituito dal backend
+				let foundRow = document.querySelector("[data-id='" + res.data.id + "']"); // utilizzo
+																							// query
+																							// selector:
+																							// mi
+																							// rende
+																							// il
+																							// primo
+																							// elemento
+																							// trovato
+				if (foundRow) { // se trovo un elemento con data-id
+					foundRow.innerHTML = ""; // pulisco la riga che ho
+												// trovato...
+					createRowElements(res.data, foundRow); // ...e poi vado a
+															// inserire il
+															// contenuto
 				
-				} else { //non ho trovato nessun data-id corrispondente; quindi si tratta di un nuvo record
-					document.getElementById("results").appendChild(createRow(res.data)); //creo una nuova riga e la appendo al result set
+				} else { // non ho trovato nessun data-id corrispondente;
+							// quindi si tratta di un nuovo record
+					document.getElementById("results").appendChild(createRow(res.data)); // creo
+																							// una
+																							// nuova
+																							// riga
+																							// e la
+																							// appendo
+																							// al
+																							// result
+																							// set
 				}	
 			
 			
@@ -73,45 +100,71 @@ function submitAuthor(event) {
 
 }
 
+function printError(error) {
+	document.getElementById("xhr-message").innerHTML = "Errore: " + error;
+}
+
+function Test(data) {
+	return `<div><h2>${data}</h2></div>`;
+}
+
 var getall = function getAll() {
-	var req = new XMLHttpRequest();
-	req.onload = function() {
-		res = JSON.parse(this.responseText);
-		if (res.status === "ok") {
-			JSON.parse(res.data).forEach(
-					function(item) {
-						document.getElementById("results").appendChild(
-								createRow(item));
-					});
-		} else {
-			document.getElementById("xhr-message").innerHTML = "Errore nel ottenere i Authors";
+	const render = function(res) {
+		try {
+			//document.querySelector("test").innerHTML = Test(res);
+			Test(res);
+			res.forEach(
+				function(item) {
+					document.getElementById("results").appendChild(
+						createRow(item));
+			});
+		} catch(error) {
+			console.log(error);
 		}
-	};
-	req.open("GET", "http://localhost:8080/" + "authors");
-	req.send();
-}();
+	}
+	// JavaScript FETCH
+	var requestOptions = {
+			  method: 'GET',
+			  mode: 'cors',
+			  cache: 'no-cache',
+			  credentials: 'omit',
+			  headers: {
+				  "Content-Type":"application/json",
+			  },
+			  redirect: 'follow'
+			};
+			
+			fetch("http://localhost:8080/authors/", requestOptions)
+			  .then(response => response.json())
+			  .then(result => render(result))
+			  .catch(error => console.log('error', error));
+	}();
 
 /**
  * Utilizzo questo metodo per creare una nuova riga
- * @param item: oggetto con i dati
+ * 
+ * @param item:
+ *            oggetto con i dati
  */
 function createRow(item) {
-	//creo una riga
+	// creo una riga
 	let row = document.createElement("div");
-	row.className = "row"; //setto la classe
-	row.setAttribute("data-id", item.id); //setto l'attrituto data-id
+	row.className = "row"; // setto la classe
+	row.setAttribute("data-id", item.id); // setto l'attrituto data-id
 	return createRowElements(item, row); 
 }
 
 function createRowElements(item,row) {
 
-	row.appendChild(createRowDataCell("id", item.id)); //aggiungo la colonna alla riga
-	row.appendChild(createRowDataCell("name", item.name));//aggiungo la colonna alla riga
+	row.appendChild(createRowDataCell("id", item.id)); // aggiungo la colonna
+														// alla riga
+	row.appendChild(createRowDataCell("name", item.name));// aggiungo la
+															// colonna alla riga
 	row.appendChild(createRowDataCell("email", item.email));
-	//creo una colonne BOTTONI
+	// creo una colonne BOTTONI
 	col = document.createElement("div");
 	col.className = "col-btn";
-	//creo un tag A	Modifica	
+	// creo un tag A Modifica
 	let updBtn = document.createElement("a");
 	updBtn.className = "update-row";
 	updBtn.setAttribute("data-id", item.id);
@@ -126,33 +179,37 @@ function createRowElements(item,row) {
 		formAuthor["name"].value = name.innerHTML;
 		formAuthor["email"].value = email.innerHTML;
 	});
-	//aggiungo il pulsante alla colonna	
+	// aggiungo il pulsante alla colonna
 	col.appendChild(updBtn);	
-	//creo un tag A	Cancella	
+	// creo un tag A Cancella
 	let delBtn = document.createElement("a");
 	delBtn.setAttribute("data-id", item.id);
 	delBtn.className = "delete-row";
 	delBtn.innerHTML="Cancella";
 	delBtn.setAttribute('href', "#");
-	//aggiungo il pulsante alla colonna	
+	// aggiungo il pulsante alla colonna
 	col.appendChild(delBtn);
-	//aggiungo la colonna alla riga	
+	// aggiungo la colonna alla riga
 	row.appendChild(col);
 	return row;
 }
 
 /**
- * crea una data cell, cioè una cella contente i dati
- * @param item: oggetto con i dati
- * @param name: nome colonna
- * @param value: valore
+ * crea una data cell, cioé una cella contente i dati
+ * 
+ * @param item:
+ *            oggetto con i dati
+ * @param name:
+ *            nome colonna
+ * @param value:
+ *            valore
  */
 function createRowDataCell(name, value) {
 	col = document.createElement("div");
 	col.className = "col";
-	col.setAttribute("data-" + name, value); //setta l'attributo data-name
-	col.innerHTML = value; //setta il contenuto visibile della cella
+	col.setAttribute("data-" + name, value); // setta l'attributo data-name
+	col.innerHTML = value; // setta il contenuto visibile della cella
 	return col;
 }
-//x
+// x
 
--- journal/src/main/java/it/esedra/journal/AuthorController.java
+++ journal/src/main/java/it/esedra/journal/AuthorController.java
@@ -1,6 +1,7 @@
 package it.esedra.journal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,15 +11,17 @@ import it.esedra.journal.entity.Author;
 import it.esedra.journal.repository.AuthorRepository;
 
 @RestController
-@RequestMapping(path="/author")
+//@RequestMapping("author")
 public class AuthorController {
 	@Autowired // This means to get the bean called userRepository
 	private AuthorRepository authorRepository;
 
-	@GetMapping(path = "/all")
+	@GetMapping("/authors")
+	@CrossOrigin
 	public @ResponseBody Iterable<Author> getAllUsers() {
 		return authorRepository.findAll();
 	}
+
 
 	
 }
